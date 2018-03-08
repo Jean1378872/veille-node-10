c7c0dea0534a1063f03e0a76d997d47b88eb5d6a
Laura Jean <e1378872@cmaisonneuve.qc.ca>
2018-03-08 11:42:46 -0500

    Chat fonctionnelle ajout de css de couleurs

--- mes_modules/chat_socket/index.js
+++ mes_modules/chat_socket/index.js
@@ -6,10 +6,11 @@ module.exports.listen = function(server){
 
     // ------------------------------ Traitement du socket
     let objUtilisateur = {}
+    let objMessage = {}
 
     io.on('connection', function(socket){
     console.log(socket.id)
-    // .......
+    // ................................. //
     socket.on('setUser', function(data){
     	objUtilisateur[socket.id] = data.user
     	console.log(util.inspect(data))
@@ -17,6 +18,21 @@ module.exports.listen = function(server){
     	io.sockets.emit('diffuser_list_user', objUtilisateur)
     })
 
+    socket.on('setMessage', function(data){
+    	data.user = objUtilisateur[socket.id]
+    	console.log(util.inspect(data))
+    	socket.emit('valide_message', data)
+    	socket.broadcast.emit('diffuser_list_message', data/*objMessage*/)
     })
+
+    socket.on('disconnect', function(data){
+    	delete objUtilisateur[socket.id]
+    	io.sockets.emit('diffuser_list_user', objUtilisateur)
+    })
+    })//fin connection
  return io
 }
+
+/// sockets -> a tout le monde
+/// broadcast socket -> tout le monde sauf moi
+/// socket -> moi
